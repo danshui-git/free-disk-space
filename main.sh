4d1129383549ec6dacb1636ab6dab1558f128ff1
danshui-git <68673669+danshui-git@users.noreply.github.com>
2025-05-26 15:32:27 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -196,7 +196,7 @@ function remove_folder(){
 
 function remove_docker_image(){
     echo "📁 Removing docker image"
-    sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc /opt/hostedtoolcache/CodeQL
+    sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc /opt/hostedtoolcache/CodeQL || true
     sudo docker image prune --all --force
 }
 
